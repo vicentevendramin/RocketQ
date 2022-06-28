56cf75b1828d8751ac3c3882a169267eee02b1c4
Vicente Vendramin <guesser.vicente@gmail.com>
2022-06-27 22:44:32 -0300

    web app v1

--- src/route.js
+++ src/route.js
@@ -7,10 +7,11 @@ const route = express.Router()
 route.get('/', (req, res) => res.render("index", {page: 'enter-room'}))
 route.get('/create-pass', (req, res) => res.render("index", {page: 'create-pass'}))
 
-route.get('/room/:room', (req, res) => res.render("room"))
-
-// Format that the form inside the modal has to pass the information
-route.post('/question/:room/:question/:action', QuestionController.index)
 route.post('/create-room', RoomController.create)
+route.get('/room/:room', RoomController.open)
+route.post('/enterroom', RoomController.enter)
+
+route.post('/question/create/:room', QuestionController.create)
+route.post('/question/:room/:question/:action', QuestionController.index)
 
 module.exports = route
